c808698b3b9843f302bef8db0bc9a9b16d3a15a9
rahul kumar <rkrahul8181@gmail.com>
2023-04-05 09:26:25 +0530

    implement getTravelFormData

--- travelopia_backend/src/controllers/travelForm.js
+++ travelopia_backend/src/controllers/travelForm.js
@@ -1,7 +1,6 @@
 const TravelForm = require('../models/travelForm.modal')
 
 const getTravelFormData = async (req, res, next) => {
-    // res.send('fine')
     try {
         const { name, email, destination, numTravellers, budgetPerPerson, currency } = req.body;
         const newTravelForm = new TravelForm({
@@ -12,13 +11,25 @@ const getTravelFormData = async (req, res, next) => {
             budgetPerPerson,
             currency,
         });
-    } catch (error) {
 
+        await newTravelForm.save();
+
+        res.status(201).send({msg:"Form submission saved sucessfully"})
+    } catch (error) {
+        next(error)
     }
 };
 
 const postTravelFormData = async (req, res, next) => {
-    res.send('fine')
+    // res.send('fine')
+    try {
+        const travelData = await TravelForm.find().sort({createdAt:"desc"});
+        res.status(200).json({
+            data
+        })
+    } catch (error) {
+        next(error)
+    }
 };
 
 module.exports = { getTravelFormData, postTravelFormData };
