96a8e3813668b9aefe30cdbc25c6170d8a1fe92b
rahul kumar <rkrahul8181@gmail.com>
2023-04-05 11:24:30 +0530

    create Navbar

--- travelopia_frontend/src/App.jsx
+++ travelopia_frontend/src/App.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import {Routes,Route} from "react-router-dom"
 import TravelFormPage from "./pages/TavelFormPage"
 import SubmissionPage from './pages/SubmissionPage'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
+    <>
+    <Navbar />
     <Routes>
       <Route path="/" element={<TravelFormPage />} />
       <Route path="/form-submission" element={<SubmissionPage />} />
     </Routes>
+    </>
   )
 }
 
--- travelopia_frontend/src/pages/TavelFormPage.jsx
+++ travelopia_frontend/src/pages/TavelFormPage.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
-import { Box } from '@chakra-ui/react'
+import { Box, Img } from '@chakra-ui/react'
 import { formPage } from '../utils/styles'
 import TravelForm from '../components/TravelForm'
+// import Navbar from '../components/Navbar'
+
+
 
 const TavelFormPage = () => {
     return (
         <>
+            {/* <Navbar /> */}
             <Box w="100vw" h="90vh" p="0" mt="-2rem"
                 clipPath={formPage.clipPath}
                 bg={formPage.linear_gradient}
             >
             </Box>
             <TravelForm />
+            
         </>
     )
 }
