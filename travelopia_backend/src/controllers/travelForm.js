e0113782af7db24976195e06362c9022cd602ef6
rahul kumar <rkrahul8181@gmail.com>
2023-04-05 09:22:47 +0530

    implement post travel form data through controllers

--- travelopia_backend/src/controllers/travelForm.js
+++ travelopia_backend/src/controllers/travelForm.js
@@ -1,11 +1,24 @@
+const TravelForm = require('../models/travelForm.modal')
 
+const getTravelFormData = async (req, res, next) => {
+    // res.send('fine')
+    try {
+        const { name, email, destination, numTravellers, budgetPerPerson, currency } = req.body;
+        const newTravelForm = new TravelForm({
+            name,
+            email,
+            destination,
+            numTravellers,
+            budgetPerPerson,
+            currency,
+        });
+    } catch (error) {
 
-const getTravelFormData = async(req,res,next)=>{
+    }
+};
+
+const postTravelFormData = async (req, res, next) => {
     res.send('fine')
 };
 
-const postTravelFormData = async(req,res,next)=>{
-    res.send('fine')
-};
-
-module.exports = {getTravelFormData,postTravelFormData};
+module.exports = { getTravelFormData, postTravelFormData };
