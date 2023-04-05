34561366a7da8ce12ea69a89b28e61a45e37c1ec
rahul kumar <rkrahul8181@gmail.com>
2023-04-05 08:38:26 +0530

    Connect to MongoDB database

--- travelopia_backend/src/server.js
+++ travelopia_backend/src/server.js
@@ -1,8 +1,17 @@
 require('dotenv').config();
 const express = require('express')
 const app = express();
+const cors = require('cors');
+
+// Database connection function
+const connect = require('./database/db')
+
 const PORT = process.env.PORT || 8080;
+
+// Default Middlewares
 app.use(express.json());
+app.use(cors())
+app.use(express.urlencoded({extended:true}))
 
 
 app.get("/",async(req,res)=>{
@@ -10,5 +19,6 @@ app.get("/",async(req,res)=>{
 });
 
 app.listen(PORT,async()=>{
+    await connect();
     console.log("listening on PORT",PORT)
 })
